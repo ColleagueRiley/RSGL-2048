bf104340330417e03e20a8aa83080ed03626578e
Colleague Riley <colleagueRiley@gmail.com>
2022-06-24 11:42:46 -0400

    added roundedRects, RSGL::buttons and fixed num placement

--- source/main.cpp
+++ source/main.cpp
@@ -26,23 +26,21 @@ bool fullBoard(){
 }
 bool running=true;
 RSGL::window win("RSGL 2048",{X,Y,LENGTH,WIDTH+40},{200,200,200});
-void sleep(float time){std::string cmd = "sleep " + std::to_string(time); system(cmd.data());}
 std::vector<RSGL::color> colormap = {{255,255,255},{252,233,79},{138,226,52},{252,175,62},{114,159,207},{173,127,168},{193,125,17},{239,41,41},{184,150,0},{78,154,6},{206,92,0},{32,71,142}};
 
+RSGL::Button b("New Game","/usr/share/fonts/TTF/SansPosterBold.ttf", {65,7,15}, {60,10,120,15},{255,0,0},{255,240,0});
 
-void eventHandler(){
-	int r;
-	win.checkEvents(); bool cnew=false;
+void eventHandler(){ win.checkEvents(); b.checkEvents();
+	bool cnew=false; int r;
+	if (b.event==1){
+		board = {};
+		for (int y=0; y < rows; y++){  board.insert(board.end(),{{}}); for (int x=0; x < collums; x++){ 
+			board.at(board.size()-1).insert(board.at(board.size()-1).end(),{{((x+5)+((LENGTH-(LENGTH/20))/100))+(((LENGTH-(LENGTH/20))/collums)*x)+(((LENGTH-(LENGTH/20))/125)*x),((y+40)+((WIDTH-(WIDTH/20))/100))+(((WIDTH-(WIDTH/20))/rows)*y)+(((WIDTH-(WIDTH/20))/125)*y),((LENGTH-(LENGTH/20))-20)/rows,((WIDTH-(WIDTH/20))-20)/rows}});}
+		}
+		for (int i=0; i<2; i++) {int x=rand() % collums, y=rand() % rows; int r=rand() % 100;  r=rand() % 100;  board.at(x).at(y).num=2; if (r >= 90) board.at(x).at(y).num=4; if (r >= 90) board.at(x).at(y).num=4;} 
+	} 
 	switch(win.event.type){
-		case RSGL::quit: running =false;
-		case RSGL::MouseButtonReleased:
-			if (RSGL::RectCollidePoint({60,10,120,15},{win.event.x,win.event.y})){
-				board = {};
-				for (int y=0; y < rows; y++){  board.insert(board.end(),{{}}); for (int x=0; x < collums; x++){ 
-					board.at(board.size()-1).insert(board.at(board.size()-1).end(),{{((x+5)+((LENGTH-(LENGTH/20))/100))+(((LENGTH-(LENGTH/20))/collums)*x)+(((LENGTH-(LENGTH/20))/125)*x),((y+40)+((WIDTH-(WIDTH/20))/100))+(((WIDTH-(WIDTH/20))/rows)*y)+(((WIDTH-(WIDTH/20))/125)*y),((LENGTH-(LENGTH/20))-20)/rows,((WIDTH-(WIDTH/20))-20)/rows}});}
-				}
-				for (int i=0; i<2; i++) {int x=rand() % collums, y=rand() % rows; int r=rand() % 100;  r=rand() % 100;  board.at(x).at(y).num=2; if (r >= 90) board.at(x).at(y).num=4; if (r >= 90) board.at(x).at(y).num=4;} 
-			}
+		case RSGL::quit: running =false; break;
 		case RSGL::KeyReleased: 
 			if (win.event.key == "Escape") running=false;
 			else if (fullBoard()){}
@@ -94,37 +92,26 @@ void eventHandler(){
 	}
 }
 
-int d(int num){if (num >= 100 && num < 1000) return 1; else if (num >= 100) return 2; return 0;}
-
+int d(int num){if (num > 100 && num < 1000) return 4; else if (num > 100) return 9;  else if (num > 10) return 3.8; return 3.5;}
 void piece::draw(){	
-	
-	if (num){ 
-		RSGL::drawRect(r,colormap.at(log(num)/log(2)));		
-		RSGL::drawText(std::to_string(num),{r.x+25 - (15*d(num)),r.y+35,r.width/3},"/usr/share/fonts/SansPosterBold.ttf",{255,255,255});
-	} else RSGL::drawRect(r,colormap.at(0));		
-	drawCir();
-}
-void piece::drawCir(){
-	RSGL::drawCircle({r.x-(r.width/10),r.y-(r.length/10),(WIDTH+LENGTH)/50},{200,200,200}); // top left
-	
-	RSGL::drawCircle({r.width+r.x,r.y-(r.length/10),(WIDTH+LENGTH)/50},{200,200,200}); // top right
-	
-	RSGL::drawCircle({r.width+r.x - 8 ,r.length+r.y -(r.width/12),(WIDTH+LENGTH)/50},{200,200,200}); //bottom right
-	RSGL::drawCircle({r.x-(r.width/10) ,r.length+r.y -(r.width/12),(WIDTH+LENGTH)/50},{200,200,200}); //bottom left
+	RSGL::drawRoundRect(r, num ? colormap.at(log(num)/log(2)) : colormap.at(0) );		
+	if (num) RSGL::drawText(std::to_string(num),{r.x+r.length/(d(num)) ,r.y+r.length/2,r.length/4},"/usr/share/fonts/TTF/SansPosterBold.ttf",{255,255,255});	
 }
  
 int main(){
-	srand(time(NULL));
+	srand(time(NULL)); // random seed for making random numbers
+	// generate the board
 	for (int y=0; y < rows; y++){  board.insert(board.end(),{{}}); for (int x=0; x < collums; x++){ 
 		board.at(board.size()-1).insert(board.at(board.size()-1).end(),{{
 			((x+5)+((LENGTH-(LENGTH/20))/100))+(((LENGTH-(LENGTH/20))/collums)*x)+(((LENGTH-(LENGTH/20))/125)*x),
 			((y+40)+((WIDTH-(WIDTH/20))/100))+(((WIDTH-(WIDTH/20))/rows)*y)+(((WIDTH-(WIDTH/20))/125)*y),((LENGTH-(LENGTH/20))-20)/rows,((WIDTH-(WIDTH/20))-20)/rows}});}
 	}
+	// add two random nums, randomly chosen to be 2 or 4 (with a higher change of it being 2)
 	for (int i=0; i<2; i++) {int x=rand() % collums, y=rand() % rows; int r=rand() % 100;  r=rand() % 100;  board.at(x).at(y).num=2; if (r >= 90) board.at(x).at(y).num=4; if (r >= 90) board.at(x).at(y).num=4;} 
 	while (running){
 		eventHandler();
 		for (int y=0; y < board.size(); y++) for (int x=0; x < board.at(y).size(); x++) board.at(y).at(x).draw();
-		RSGL::drawRect({60,10,120,15},{255,240,0}); RSGL::drawText("New Game", {60,10,15},"/usr/share/fonts/SansPosterBold.ttf",{255,0,0}); 
+		b.draw();
 		win.clear();
 	} win.close();
 }
